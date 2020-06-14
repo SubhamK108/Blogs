7044902b557ffde2633185885ca840cfe1f4c5a3
Subham Karmakar <subhamk.subs@gmail.com>
2020-06-14 08:35:55 +0530

    Changes the dark mode toggle

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -11,16 +11,16 @@ function toggleDarkMode() {
     element.classList.toggle("dark");
 
     if (document.getElementById("main")) {
-        document.getElementById("main").id = "dark";
+        document.getElementById("main").id = "dark-main";
     } else {
-        document.getElementById("dark").id = "main";
+        document.getElementById("dark-main").id = "main";
     }
 
     var sec = document.getElementsByClassName("special")
     for (var i = 0; i < sec.length; i++)
     {
         if (sec[i].className == "main special") {
-            sec[i].className = "dark special";
+            sec[i].className = "dark-main special";
         } else {
             sec[i].className = "main special";
         }
